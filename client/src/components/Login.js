60236acebc51d17cf88a5469ba3021dbcb443f29
Yixuan Sally Zhang <sallyhoney96@gmail.com>
2025-03-12 08:36:20 -0400

    Updated api base url

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,7 @@ import { Box, FormControl, FormLabel, Input, Button, Heading, useToast, Spinner 
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = 'https://workforce-tracker-backend.onrender.com';
 
 const Login = () => {
   const [email, setEmail] = useState('');
